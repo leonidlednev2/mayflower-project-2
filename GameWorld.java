a98422d74e782c3dc23ba2b6998131fc0683105b
Leonid <leonidledn@gmail.com>
2024-04-12 09:50:03 -0500

    add default `README.TXT` to ignore

---
+++ GameWorld.java
@@ -0,0 +1,14 @@
+import mayflower.*;
+
+public class GameWorld extends World {
+    private Actor f1;
+    private Actor f2;
+    public GameWorld() {
+        f1 = new Player();
+        f2 = new Enemy();
+    }
+    
+    public void act() {
+        
+    }
+}
--- TitleScreen.java
+++ TitleScreen.java
@@ -5,15 +5,14 @@ public class TitleScreen extends World
     public TitleScreen()
     {
         // setBackground("img/BG/BG.png");
-        showText("Lorem ipsum",10,30,Color.BLACK);
-        showText("Lorem ipsum" , 10, 70, Color.BLACK);
-        //showText("You can also collect hearts to increase your lives. If you touch a spike you lose a life and reset locations. If you touch a cloud or fall of the screen you die" , 10 , 70, Color.BLACK);
+        showText("Stick fighting game",10,30,Color.BLACK);
+        showText("Press enter/return to start" , 10, 70, Color.BLACK);
 
     }
 
     public void act()
     {
-        /*if (Mayflower.isKeyDown(Keyboard.KEY_UP))
+        /*if (Mayflower.isKeyDown(Keyboard.KEY_ENTER))
         {
             GameWorld g = new GameWorld();
             Mayflower.setWorld(new GameWorld());
