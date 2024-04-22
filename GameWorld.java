8f32ad468562c6a3aef92d0dd66fe384a2d1906e
Leonid <leonidledn@gmail.com>
2024-04-22 09:39:17 -0500

    add actors to `GameWorld.java`

--- GameWorld.java
+++ GameWorld.java
@@ -9,6 +9,9 @@ public class GameWorld extends World {
         p = new Player();
         e = new Enemy();
         setBackground("img/SF_bg.jpg");
+
+        addObject(p, 100, 100);
+        addObject(e, 100, 700);
     }
 
     public void act() {
